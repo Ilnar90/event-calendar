06990b9ae124c9af91359abdd36fd89359d10839
ilnar <your.email@example.com>
2014-01-08 16:25:24 +0400

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Rustam::Application.config.secret_token = '402238487c5c268bd86bf7809f2cd3fb3b827a02e04f0eee2f437fe7896713220fb360ee0310929731ecff09561f95a20c4e2cd5187b03477cbb4c84a86bc42e'
