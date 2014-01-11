3f3b0ae18a6abf0f1c2690a308a583ead52cb1d3
ilnar <your.email@example.com>
2014-01-11 12:40:37 +0400

    Add bootstrap and pages

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rustam::Application.routes.draw do
+  get "pages/index"
+
   devise_for :users
+  
 
   # The priority is based upon order of creation:
   # first created -> highest priority.
@@ -50,7 +53,7 @@ Rustam::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-  # root :to => 'welcome#index'
+   root :to => 'pages#index'
 
   # See how all your routes lay out with "rake routes"
 
