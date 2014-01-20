af17b43fc5665784b12957cc3473df3aea643ac4
Ildus Sadykov <ildus.sadykov@flatstack.com>
2014-01-20 02:03:12 -0500

    some fixes

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,28 +1,28 @@
 class EventsController < ApplicationController
-  def index  
-    @events = Event.scoped  
-    @events = Event.between(params['start'], params['end']) 
-    if params['start'] && params['end']  
-      respond_to do |format|  
-      format.html # index.html.erb  
-      format.json { render :json => @events }  
-    end  
+  respond_to :html, :json
+  def index
+    @events = Event.scoped
+    @events = Event.between(params['start'], params['end']) if params['start'] && params['end']
+      respond_to do |format|
+      format.html # index.html.erb
+      format.json { render :json => @events }
+    end
   end
 
   def edit
   end
-  
+
   def update
   	@events = Event.find(params[:id])
     if@events.update(events_params)
       redirect_to @events
     else
       render 'edit'
-    end  
-  end 
+    end
+  end
 
   def show
     @event = Event.find(params[:id])
     respond_with @event
   end
-end  
+end
