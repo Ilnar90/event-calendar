dc1d65ff04695f4a0fa4903b022cc24a9af250b1
Ildus Sadykov <ildus.sadykov@flatstack.com>
2014-01-20 06:40:12 -0500

    some fixes

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -3,13 +3,15 @@ class EventsController < ApplicationController
   def index
     @events = Event.scoped
     @events = Event.between(params['start'], params['end']) if params['start'] && params['end']
-      respond_to do |format|
+    respond_with do |format|
       format.html # index.html.erb
-      format.json { render :json => @events }
+      format.json { render json: @events }
     end
   end
 
   def edit
+    @event = Event.find(params[:id])
+    respond_with @event
   end
 
   def update
